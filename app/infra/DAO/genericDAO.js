6863e9c4431d367b856e69f3f1440f95a61b8244
Luís Henrique Tessaro Pedroso <lh.pedrosot@hotmail.com>
2019-03-08 19:11:49 -0300

    delete by parameter

--- app/infra/DAO/genericDAO.js
+++ app/infra/DAO/genericDAO.js
@@ -9,7 +9,20 @@ player_dao.prototype.create = (obj, dbName, tableName) => {
     const dbo = db.db(dbName)
     dbo.collection(tableName).insertOne(obj, (err, res) => {
       if (err) throw err
-      console.log('Generic create Success' + res)
+      console.log('Generic create Success ' + res)
+      db.close()
+    })
+  })
+}
+
+player_dao.prototype.deleteByParameter = (name, dbName, tableName) => {
+  MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+    if (err) throw err
+    const dbo = db.db(dbName)
+    const myquery = { name: name }
+    dbo.collection(tableName).deleteOne(myquery, (err, res) => {
+      if (err) throw err
+      console.log('Generic delete Success ' + res)
       db.close()
     })
   })
