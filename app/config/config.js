5a5fd4f9dd62c0ee2009dc29ef6f441998f86930
Luís Henrique Tessaro Pedroso <lh.pedrosot@hotmail.com>
2019-03-08 18:52:42 -0300

    requiring consign

--- app/config/config.js
+++ app/config/config.js
@@ -1,8 +1,14 @@
 const puppeteer = require('puppeteer')
 const TeleBot = require('telebot')
 const token = require('./key.json')
+const load = require('consign')
 
 module.exports = function () {
   var bot = new TeleBot(token.key)
+
+  load({ cwd: 'app' })
+    .include('routes')
+    .into(bot)
+
   return bot
 }
