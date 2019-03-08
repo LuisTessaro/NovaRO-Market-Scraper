d69444ad597faabe1e73339e9b88e156efea5ff7
Luís Henrique Tessaro Pedroso <lh.pedrosot@hotmail.com>
2019-03-08 18:47:38 -0300

    misplaced key

--- app/config/config.js
+++ app/config/config.js
@@ -1,9 +1,8 @@
 const puppeteer = require('puppeteer')
 const TeleBot = require('telebot')
 const token = require('./key.json')
-const bot = new TeleBot(token.key)
 
 module.exports = function () {
-  var bot = new TeleBot(token)
+  var bot = new TeleBot(token.key)
   return bot
 }
